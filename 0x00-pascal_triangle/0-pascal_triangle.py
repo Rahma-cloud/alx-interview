76e9f81fe53bf523d28a24d7ecebf8014e8ee279
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-04 19:32:40 +0100

    Update pascal file

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-pascal triangle module
+12-pascal_triangle Module
 """
 
 
 def pascal_triangle(n):
-    """function of pascal triangle"""
+    """
+    pascal_triangle function
+    """
+    my_list = []
     if n <= 0:
-        return 0
-    triangle = [[1]]
-    for _ in range(1, n):
-        row = [1]
-        for j in range(1, len(triangle[-1])):
-            row.append(triangle[-1][j - 1] + triangle[-1][j])
-        row.append(1)
-        triangle.append(row)
+        return my_list
 
-    return triangle
+    for i in range(n):
+        row = [1]
+        if len(my_list) > 0:
+            prev_row = my_list[-1]
+            for j in range(len(prev_row) - 1):
+                row.append(prev_row[j] + prev_row[j + 1])
+            row.append(1)
+        my_list.append(row)
+    return my_list
