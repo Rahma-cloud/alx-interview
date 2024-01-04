b9c1e2db4e8ae53eec88485c07439c1c35e06f9d
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-04 08:58:57 +0100

    Add pascalTriangle files

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,14 @@
-def pascal_triangle(n):
-    if n <= 0: 
-        return 0
-    triangle = [[1]]
-    for _ in range(1, n):
-        row = [1]
-        for j in range(1, len(triangle[-1])):
-            row.append(triangle[-1][j - 1] + triangle[-1][j])
-        row.append(1)
-        triangle.append(row)
+if __name__ == "__main__":
 
-    return triangle
+    def pascal_triangle(n):
+        if n <= 0:
+            return 0
+        triangle = [[1]]
+        for _ in range(1, n):
+            row = [1]
+            for j in range(1, len(triangle[-1])):
+                row.append(triangle[-1][j - 1] + triangle[-1][j])
+            row.append(1)
+            triangle.append(row)
+
+        return triangle
