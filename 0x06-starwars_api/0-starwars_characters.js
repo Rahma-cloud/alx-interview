5ac51ab6d7a53cd3ca7a98b46603e79d754ac8d1
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-02-13 15:03:29 +0100

    update tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,29 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-const movieId = process.argv[2];
-
-// Fetch movie details from the Star Wars API
-const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, (error, response, body) => {
+request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+    sendRequest(characterList, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode);
-    process.exit(1);
-  }
-
-  const movie = JSON.parse(body);
-
-  // Display characters in the order specified by the 'characters' list
-  movie.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character details:', charError);
-        process.exit(1);
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Invalid status code for character details:', charResponse.statusCode);
-        process.exit(1);
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    });
-  });
 });
