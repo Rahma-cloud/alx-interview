ca65af20746530191679f55d2f4d774f26bb51e1
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-02-20 10:16:15 +0100

    Update task file to correct pycodestyle

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,7 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
@@ -17,6 +18,7 @@ def rotate_2d_matrix(matrix):
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
+
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
