da03f6a66fd03e0b21f7cff437a8bef18d8a5368
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-03-06 11:45:50 +0100

    Add island perimeter tasks directory and file

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Island peerimeter task
+"""
+
+
+def island_perimeter(grid):
+    """ island perimeter function """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+                # Check top neighbor
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+    return perimeter
